312f490f0a93f386a1f5f466921bc29282652c02
BrianHGrant <bhgrant8@gmail.com>
2016-09-10 09:30:53 -0700

    add optional functionality for selecting contact when adding job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ActiveRecord::Base
   validates_presence_of :title, :post_link, :closing_date, :posting_date, :priority
   belongs_to :company
-  belongs_to :contact
+  belongs_to :contact, optional: true
 end
