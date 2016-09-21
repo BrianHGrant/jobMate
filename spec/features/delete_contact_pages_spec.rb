80d81e95a3cd4b10b7d63d39678e2b9b4330bde7
BrianHGrant <bhgrant8@gmail.com>
2016-09-20 17:06:02 -0700

    prepare for herkou deploy

--- spec/features/delete_contact_pages_spec.rb
+++ spec/features/delete_contact_pages_spec.rb
@@ -1,13 +1,11 @@
-require 'rails_helper'
+rairequire 'rails_helper'
 
 describe 'the delete a contact process' do
   it "deletes a contact" do
     FactoryGirl.create(:quote)
-    user = FactoryGirl.create(:user)
-    login_as(user, :scope => :user)
-    company = FactoryGirl.create(:company, user: user)
-    contact = FactoryGirl.create(:contact, company: company, user: user)
-    visit contacts_path
+    company = FactoryGirl.create(:company)
+    contact = FactoryGirl.create(:contact, company: company)
+    visit companies_path
     click_on "Castillo, Judy"
     click_on "Delete"
     expect(page).to have_no_content "Castillo, Judy"
