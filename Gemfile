e632d0c3e35fc3aa38d75566268bf56e9ba7e2cf
BrianHGrant <bhgrant8@gmail.com>
2016-09-23 08:41:41 -0700

    add ffaker gem and an example user with companies, jobs, and contacts to db:seed file, assist with testing and demo of project

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'devise'
+gem 'ffaker', :git => 'https://github.com/ffaker/ffaker.git'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -57,4 +58,4 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-ruby "2.2.4"
+ruby "> 2.2.3"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'ffaker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,3 +9,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 quotes = Quote.create([{ content: 'The most common way people give up their power is by thinking they don’t have any.', author: 'Alice Walker'}, {content: 'Our finest moments are most likely to occur when we are feeling deeply uncomfortable, unhappy, or unfulfilled. For it is only in such moments, propelled by our discomfort, that we are likely to step out of our ruts and start searching for different ways or truer answers.', author: 'M. Scott Peck'}, {content: 'If the wind will not serve, take to the oars.', author: 'Latin Proverb'}, {content: 'Find out what you like doing best, and get someone to pay you for it.', author: 'Katharine Whitehorn'}, {content: 'Go where you are celebrated—not tolerated. If they can not see the real value of you, it is time for a new start.', author: 'Unknown'}, {content: 'Wanting to be someone else is a waste of the person you are.', author: 'Marilyn Monroe'}, {content: 'The future depends on what you do today.', author: 'Mahatma Gandhi'}, {content: 'You only live once, but if you do it right, once is enough.', author: 'Mae West'}, {content: 'I am not a product of my circumstances. I am a product of my decisions.', author: 'Stephen Covey'}, {content: 'Opportunities do not happen, you create them.', author: 'Chris Grosser'}])
+
+
+User.create({email: 'john@test.com', password: '123456'})
+
+30.times do
+  Company.create({name: FFaker::Company.name, address: FFaker::AddressUS.street_address, category: FFaker::Skill.specialty, rating: rand(1..5), user_id: 1})
+end
+
+30.times do
+  Contact.create({first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, phone: FFaker::PhoneNumber.short_phone_number, email: FFaker::Internet.email, relationship: (['professional', 'friend', 'teacher', 'mentor', 'family']).sample, position: FFaker::Company.position, user_id: 1, company_id: rand(1..30), linkedIn: 'http://www.linkedIn.com/' })
+end
+
+60.times do
+  Job.create({title: FFaker::Job.title, post_link: FFaker::Internet.http_url, closing_date: FFaker::Time.between(30.days.ago, 30.days.from_now), posting_date: FFaker::Time.between(30.days.ago, Time.now), priority: rand(3..10), user_id: 1, company_id: rand(1..30), contact_id: rand(1..30) })
+end
